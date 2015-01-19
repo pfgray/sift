d86069d9d6d650c001b4d0f6b0ed23820553cc57
Paul Gray <pfbgray@gmail.com>
2015-01-19 11:21:16 -0500

    Switching to html5 urls.... is there anything better?

--- client/scripts/components/main.js
+++ client/scripts/components/main.js
@@ -15,6 +15,6 @@ var routes = (
   </Route>
 );
 
-Router.run(routes, function (Handler) {
+Router.run(routes, Router.HistoryLocation, function (Handler) {
   React.render(<Handler/>, content);
 });
