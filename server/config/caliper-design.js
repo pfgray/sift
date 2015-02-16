6a3b2fb100512845b07d36087597f011497d7b96
Paul Gray <pfbgray@gmail.com>
2015-02-16 13:27:38 -0500

    Adding group by type query

---
+++ server/config/caliper-design.js
@@ -0,0 +1,74 @@
+module.exports = {
+  _id:"_design/caliper",
+  language:"javascript",
+  views:{
+    events:{
+      map: function(doc){
+        if(doc.type === 'caliperEvent'){
+          emit(doc._id, doc);
+        }
+      }
+    },
+    users: {
+      map: function(doc){
+        if(doc.type === 'user'){
+          if(doc.username){
+            emit({username:doc.username}, doc);
+          }
+          if(doc.googleId){
+            emit({googleId:doc.googleId}, doc);
+          }
+        }
+      }
+    },
+    apiKeys: {
+      map: function(doc){
+        if(doc.type === 'user'){
+          if(doc.apiKey){
+            emit(doc.apiKey, doc);
+          }
+        }
+      }
+    },
+    events_by_user: {
+      map: function(doc){
+        if(doc.type === 'caliperEvent'){
+          emit([doc.scoped_caliper_user_id, doc.recieved], doc);
+        }
+      },
+      reduce: function (key, values, rereduce) {
+        if (rereduce) {
+          return sum(values);
+        } else {
+          return values.length;
+        }
+      }
+    },
+    events_by_type: {
+      map: function(doc){
+        if(doc.type === 'caliperEvent'){
+          emit([doc.scoped_caliper_user_id, doc.recieved, doc.caliperObject['@type']], doc);
+        }
+      },
+      reduce: function (key, values, rereduce) {  if (rereduce) {    return sum(values);  } else {    return values.length;  }}
+    }
+  },
+  lists:{
+    group_by_type: function(head, req){
+      provides('json', function(){
+        var EVENT_TYPE_INDEX = 2;
+        var results = {};
+        while (row = getRow()) {
+          var count = 0;
+          var eventType = row.key[EVENT_TYPE_INDEX];
+          if(typeof results[eventType] === 'undefined'){
+              results[eventType] = 0;
+          }
+          results[eventType] += 1;
+        }
+
+        return JSON.stringify(results);
+      });
+    }
+  }
+}
